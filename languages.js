0312683e5c0b8ee94923964b2eeebc106798aedd
Julian <julianbenjamingauna@gmail.com>
2025-02-28 18:06:33 -0300

    Update languages.js

--- languages.js
+++ languages.js
@@ -11,21 +11,6 @@ const translations = {
         "name-label": "Name:",
         "comment-label": "Comment:",
         "submit-button": "Submit",
-        "faq-title" : "Frequently Asqued Questions";
-        "faq1" : "What is your experience level?";
-        "faqa1" : "I am constantly learning and growing, currently at an intermediate level, working on various personal and freelance projects.";
-        "faq2" : "What languages and frameworks do you use?";
-        "faqa2" : "I primarily work with JavaScript, React, HTML, CSS, and Git for version control. I am also learning TypeScript.";
-        "faq3" : "Do you offer freelance work?";
-        "faqa3" : "Yes! Feel free to contact me via email or Discord for freelance opportunities.";
-        "pricing" : "Pricing"
-        "prices" : "Prices vary depending on the complexity of the project. However, they generally range from $5 to $50. Feel free to contact me for a more accurate quote based on your needs!"
-        "r1" : '"Exceptional work, always delivered on time and exceeding expectations."'
-        "r2" : '"Great professionalism and always willing to listen to our needs."'
-        "r3" : '"The best in his area. I recommend his work without reservation."'
-        "r4" : '"Incredible attention and always delivering quality work, we will definitely work with him again!"'
-        "contact" : "Contact Me"
-        "contact2" : "If you'd like to work with me, feel free to reach out!"
     },
     es: {
         "header-title": "Julian",
@@ -39,21 +24,6 @@ const translations = {
         "name-label": "Nombre:",
         "comment-label": "Comentario:",
         "submit-button": "Enviar",
-        "faq-title" : "Preguntas Frecuentes"
-        "faq1" : "Cual es tu nivel de experiencia?"
-        "faqa1" : "Estoy en constante aprendizaje y crecimiento, actualmente en un nivel intermedio, trabajando en diversos proyectos personales y freelance.";
-        "faq2" : "Que lenguajes y frameworks usas?"
-        "faqa2" : "Trabajo principalmente con JavaScript, React, HTML, CSS y Git para el control de versiones. También estoy aprendiendo TypeScript.";
-        "faq3" : "Ofreces trabajo independiente?"
-        "faqa3" : "¡Sí! No dudes en contactarme por correo electrónico o Discord para oportunidades de trabajo independiente.";
-        "pricing" : "Precios"
-        "prices" : "Los precios varían dependiendo de la complejidad del proyecto. Sin embargo, generalmente oscilan entre $5 y $50. No dude en ponerse en contacto conmigo para obtener una cotización más precisa según sus necesidades."
-        "r1" : '"Trabajo excepcional, siempre entregado a tiempo y superando las expectativas."'
-        "r2" : '"Gran profesionalidad y siempre dispuesto a escuchar nuestras necesidades."'
-        "r3" : '"El mejor en su área. Recomiendo su trabajo sin reservas."'
-        "r4" : '"Increíble atención y siempre entregando un trabajo de calidad, ¡definitivamente volveremos a trabajar con él!"'
-        "contact" : "Contactame"
-        "contact2" : "Si quieres trabajar conmigo, ¡no dudes en comunicarte!"
     },
     ja: {
         "header-title": "ジュリアン",
@@ -67,21 +37,6 @@ const translations = {
         "name-label": "名前:",
         "comment-label": "コメント:",
         "submit-button": "送信",
-        "faq-title" : "よくある質問";
-        "faq1" : "あなたの経験レベルはどれくらいですか？"
-        "faqa1" : "私は常に学び、成長しており、現在は中級レベルで、さまざまな個人プロジェクトやフリーランスのプロジェクトに取り組んでいます。";
-        "faq2" : "どのような言語とフレームワークを使用していますか?"
-        "faqa2" : "私は主に JavaScript、React、HTML、CSS、および Git を使用してバージョン管理を行っています。 TypeScriptも勉強中です。";
-        "faq3" : "フリーランスの仕事を提供していますか?";
-        "faqa3" : "はい！フリーランスの機会については、電子メールまたは Discord でお気軽にご連絡ください。";
-        "pricing" : "価格"
-        "prices" : "価格はプロジェクトの複雑さによって異なります。ただし、通常は 5 ドルから 50 ドルの範囲です。お客様のニーズに基づいてより正確なお見積りをご希望の場合は、お気軽にお問い合わせください。"
-        "r1" : "「優れた仕事、常に予定通りに納品され、期待を上回っています。」"
-        "r2" : "「素晴らしいプロ意識と、常に私たちのニーズに耳を傾けてくれます。」"
-        "r3" : "「彼の分野では最高です。私は彼の作品を遠慮なくお勧めします。」"
-        "r4" : "「信じられないほどの配慮と常に質の高い仕事を提供してくれるので、間違いなくまた彼と仕事をするでしょう！」"
-        "contact" : "連絡してください"
-        "contact2" : "私と一緒に働きたいと思ったら、遠慮せずにご連絡ください!"
     }
 };
 
